49b29ab2a424d57a16d4ef351d9447e02ec747d3
shamyorjani <shamyorjani@gmail.com>
2024-03-04 12:40:35 +0500

    API URL error was accure

---
+++ static/connect.js
@@ -0,0 +1,303 @@
+// API Controller Module
+const APIController = (function () {
+  const _getConnectedUserPlaylists = async (accessToken) => {
+    const result = await fetch("https://api.spotify.com/v1/me/playlists", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+    });
+
+    const data = await result.json();
+    return data.items;
+  };
+
+  const _getTopTracks = async (accessToken) => {
+    const result = await fetch("https://api.spotify.com/v1/me/top/tracks", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+    });
+
+    const data = await result.json();
+    return data.items;
+  };
+  const _getUserProfile = async (accessToken) => {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+    });
+
+    const data = await result.json();
+    return data;
+  };
+  const _getRecentlyPlayedTracks = async (accessToken) => {
+    const result = await fetch(
+      "https://api.spotify.com/v1/me/player/recently-played",
+      {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + accessToken,
+        },
+      }
+    );
+
+    const data = await result.json();
+    return data;
+  };
+
+  // Add more private methods for additional functionalities
+
+  return {
+    getConnectedUserPlaylists(accessToken) {
+      return _getConnectedUserPlaylists(accessToken);
+    },
+
+    getTopTracks(accessToken) {
+      return _getTopTracks(accessToken);
+    },
+    getUserProfile(accessToken) {
+      return _getUserProfile(accessToken);
+    },
+    getRecentlyPlayedTracks(accessToken) {
+      return _getRecentlyPlayedTracks(accessToken);
+    },
+
+    // Add more public methods for additional functionalities
+  };
+})();
+
+// UI Module
+const UIController = (function () {
+  const DOMElements = {
+    connectBtn: "#connectBtn",
+    playlistsContainer: "#userPlaylists",
+    topTracksContainer: "#topTracks",
+    userProfileContainer: "#userProfile",
+    recentlyPlayedContainer: "#recentlyPlayedContainer",
+    togglePlaying: ".togglePlay",
+    // Add more selectors as needed
+    // Add more selectors as needed
+  };
+  const attachPlayTrackEvent = function (trackItem, track, accessToken) {
+    trackItem.addEventListener("click", function () {
+      // Call a function to play the selected track
+      playTrack(accessToken, track.uri);
+    });
+  };
+  const playTrack = function (accessToken, trackUri) {
+    const player = new Spotify.Player({
+      name: "Your App Name",
+      getOAuthToken: (cb) => {
+        cb(accessToken);
+      },
+    });
+    // window.onSpotifyWebPlaybackSDKReady = (accessToken, trackUri) => {
+    //   const player = new Spotify.Player({
+    //     name: "Web Playback SDK Quick Start Player",
+    //     getOAuthToken: (cb) => {
+    //       cb(accessToken);
+    //     },
+    //     volume: 0.5,
+    //   });
+
+    // Error handling
+    player.addListener("initialization_error", ({ message }) => {
+      console.log("initilizaion error");
+      console.error(message);
+    });
+    player.addListener("authentication_error", ({ message }) => {
+      console.log("authentican error" + accessToken + " " + trackUri);
+      console.error(message);
+    });
+    player.addListener("account_error", ({ message }) => {
+      console.error(message);
+    });
+    player.addListener("playback_error", ({ message }) => {
+      console.error(message);
+    });
+
+    // Playback status updates
+    player.addListener("player_state_changed", (state) => {
+      console.log(state);
+    });
+
+    // Ready
+    player.addListener("ready", ({ device_id }) => {
+      console.log("Ready with Device ID", device_id);
+      // Play the selected track
+      playOnDevice(accessToken, device_id, trackUri);
+    });
+
+    // Connect to the player
+    player.connect().then((success) => {
+      if (success) {
+        console.log("The Web Playback SDK successfully connected to Spotify!");
+      }
+    });
+    document.querySelector(".togglePlay").addEventListener("click", () => {
+      player.togglePlay().then(() => {
+        console.log("Toggled playback!");
+      });
+    });
+
+    // Spotify Web Playback SDK ready callback
+  };
+  window.onSpotifyWebPlaybackSDKReady = () => {
+    console.log("Spotify Web Playback SDK is ready.");
+  };
+
+  const playOnDevice = async function (accessToken, deviceId, trackUri) {
+    const playEndpoint = `https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`;
+
+    const result = await fetch(playEndpoint, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        uris: [trackUri],
+      }),
+    });
+
+    const data = await result.json();
+    console.log(data);
+  };
+
+  return {
+    inputField: function () {
+      return {
+        connectBtn: document.querySelector(DOMElements.connectBtn),
+        playlistsContainer: document.querySelector(
+          DOMElements.playlistsContainer
+        ),
+        topTracksContainer: document.querySelector(
+          DOMElements.topTracksContainer
+        ),
+        userProfileContainer: document.querySelector(
+          DOMElements.userProfileContainer
+        ),
+        recentlyPlayedContainer: document.querySelector(
+          DOMElements.recentlyPlayedContainer
+        ),
+        togglePlaying: document.querySelector(DOMElements.togglePlaying),
+        // Add more selectors as needed
+      };
+    },
+
+    // UI-related methods for displaying information
+    displayUserPlaylists: function (playlists) {
+      const playlistsContainer = document.querySelector(
+        DOMElements.playlistsContainer
+      );
+      playlistsContainer.innerHTML = ""; // Clear existing content
+
+      playlists.forEach((playlist) => {
+        const playlistItem = document.createElement("div");
+        playlistItem.innerHTML = `<p>${playlist.name}</p>`;
+        playlistsContainer.appendChild(playlistItem);
+      });
+    },
+
+    displayTopTracks: function (topTracks) {
+      const topTracksContainer = document.querySelector(
+        DOMElements.topTracksContainer
+      );
+      topTracksContainer.innerHTML = ""; // Clear existing content
+
+      topTracks.forEach((track) => {
+        const trackItem = document.createElement("div");
+        trackItem.innerHTML = `<p>${track.name} by ${track.artists[0].name}</p>`;
+        topTracksContainer.appendChild(trackItem);
+      });
+    },
+    displayUserProfile: function (userProfile) {
+      const userProfileContainer = document.querySelector(
+        DOMElements.userProfileContainer
+      );
+      userProfileContainer.innerHTML = ""; // Clear existing content
+
+      const profileItem = document.createElement("div");
+      if (userProfile.images) {
+        profileItem.innerHTML = `
+                <p>Display Name: ${userProfile.display_name}</p>
+                '<img src="${userProfile.images[0].url}" alt="Profile Image">'<br/>
+                <a href="${userProfile.external_urls.spotify}">User Profile Link</a>
+                `;
+      }
+      userProfileContainer.appendChild(profileItem);
+      //   console.log(userProfile);
+    },
+    displayRecentlyPlayedTracks: function (recentlyPlayedTracks, accessToken) {
+      const recentlyPlayedContainer = document.querySelector(
+        DOMElements.recentlyPlayedContainer
+      );
+      recentlyPlayedContainer.innerHTML = ""; // Clear existing content
+
+      recentlyPlayedTracks.items.forEach((track) => {
+        const trackItem = document.createElement("div");
+        trackItem.innerHTML = `<p>${track.track.name} by ${track.track.artists[0].name}</p>`;
+        recentlyPlayedContainer.appendChild(trackItem);
+
+        // Make the track clickable
+        // console.log(recentlyPlayedTracks);
+        // console.log(accessToken);
+        attachPlayTrackEvent(trackItem, track.track, accessToken);
+      });
+    },
+
+    // Add more UI-related methods for additional functionalities
+  };
+})();
+
+// APP Controller Module
+const APPController = (async function (UICtrl, APICtrl) {
+  const DOMInputs = UICtrl.inputField();
+
+  DOMInputs.connectBtn.addEventListener("click", connectToSpotify);
+
+  async function connectToSpotify() {
+    const clientId = "649894e3570a4a26a5e7421cc100b57e";
+    const redirectUri = "http://localhost:5000";
+    const scope =
+      "playlist-read-private user-top-read user-read-private user-read-email user-read-recently-played user-modify-playback-state user-read-playback-state streaming"; // Add additional scopes if needed
+
+    const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token`;
+
+    window.location.href = authUrl;
+  }
+
+  const accessToken = new URLSearchParams(
+    window.location.hash.substring(1)
+  ).get("access_token");
+
+  if (accessToken) {
+    const playlists = await APICtrl.getConnectedUserPlaylists(accessToken);
+    UICtrl.displayUserPlaylists(playlists);
+
+    const topTracks = await APICtrl.getTopTracks(accessToken);
+    UICtrl.displayTopTracks(topTracks);
+
+    const userProfile = await APICtrl.getUserProfile(accessToken);
+    UICtrl.displayUserProfile(userProfile);
+
+    const recentlyPlayedTracks = await APICtrl.getRecentlyPlayedTracks(
+      accessToken
+    );
+    UICtrl.displayRecentlyPlayedTracks(recentlyPlayedTracks, accessToken);
+
+    // Add more code to handle other functionalities
+  }
+
+  return {
+    init: function () {
+      console.log("App is starting");
+    },
+  };
+})(UIController, APIController);
+
+// APPController.init();
